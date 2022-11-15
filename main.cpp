04a7920ea04eedfa52cb7e13d2dfdcf8570c7418
Fleapous <girayegecan02@gmail.com>
2022-11-15 15:00:12 +0100

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <string>
+#include <iterator>
+using namespace std;
+
+vector<pair<char,int>> sortArray(string* str){
+    int n = str->length();
+    char char_array[n + 1];
+    strcpy(char_array, str->c_str());
+    int lenght = sizeof(char_array)/sizeof(char);
+
+    vector<pair<char,int>> mainVec;
+    vector<char> chechkVec;
+    vector<char>::iterator it;
+    for (int i = 0; i < lenght; ++i) {
+        it = find(chechkVec.begin(), chechkVec.end(), char_array[i]);
+        if (it != chechkVec.end()){
+            ++mainVec[it - chechkVec.begin()].second;
+//            cout << mainVec[it - chechkVec.begin()].first << " " << mainVec[it - chechkVec.begin()].second << endl;
+        }
+        else{
+            chechkVec.push_back(char_array[i]);
+            pair<char, int> tmp2;
+            tmp2.first = char_array[i];
+            tmp2.second = 1;
+            mainVec.push_back(tmp2);
+        }
+    }
+    return mainVec;
+}
+
+int main () {
+    string myText;
+    vector<pair<char,int>> str;
+    ifstream MyReadFile("test.txt");
+    getline(MyReadFile,myText);
+    cout << myText << endl;
+    MyReadFile.close();
+
+    str = sortArray(&myText);
+    for(int i = 0; i < str.size(); ++i)
+    {
+        cout << str[i].first << ", " << str[i].second << endl;
+    }
+    return 0;
+}
